6305a12ea8a43c35e332ce5b5bd15e8fb0948f8f
minyongP <86906386+Minon98@users.noreply.github.com>
2023-05-08 23:33:06 +0900

    Update Main.java

--- codingtest/src/Main.java
+++ codingtest/src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        Runnable task1 = new ThreadTask1();
+        Thread thread1 = new Thread(task1);
 
-        System.out.println("Hello world!");
+        thread1.start();
+
+        // 반복문 추가
+        for (int i = 0; i < 100; i++) {
+            System.out.print("@");
+        }
+    }
+}
+
+class ThreadTask1 implements Runnable {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.print("#");
+        }
     }
 }
