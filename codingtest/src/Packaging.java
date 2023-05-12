a3a4acfac28149f6d1c975eb0131113d088a5ba5
minyongP <86906386+Minon98@users.noreply.github.com>
2023-05-12 22:26:05 +0900

    Create Packaging.java

---
+++ codingtest/src/Packaging.java
@@ -0,0 +1,14 @@
+public class Packaging {
+    public static void main(String[] args) {
+        int[] boxes = new int[]{5, 1, 4, 6};
+        int output = paveBox(boxes);
+        System.out.println(output); // 3
+
+        int[] boxes2 = new int[]{1, 5, 7, 9};
+        int output2 = paveBox(boxes2);
+        System.out.println(output2); // 1
+    }
+    public static int paveBox(int[] boxes) {
+        for (int i : boxes);
+    }
+}
