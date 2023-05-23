e67e4b4b1ffabee7d0a43461035f1cac585c5761
minyongP <86906386+Minon98@users.noreply.github.com>
2023-05-23 23:02:14 +0900

    Create P12891.java

---
+++ codingtest/src/datastructure/slidingwindow/P12891.java
@@ -0,0 +1,30 @@
+package datastructure.slidingwindow;
+
+public class P12891 {
+    public static void main(String[] args) {
+        /*
+        * S(DNA 문자열 길이) P(부분 문자열 길이)
+        * arr(DNA 배열) checkArr({‘A’, ‘C’, ‘G’, ‘T’} 검증 배열)
+        * myArr(슬라이딩 윈도우)
+        * check(몇개 충족했는지 검사)
+        * result(비밀번호 종류 수)
+        * for(i:P~S)
+        * {
+        *   j (i - P)
+        *   //
+        *   한칸씩 이동
+        *   if(check == 4) result++
+        * }
+        * 결과 반환
+        *
+        * Add(문자 추가)
+        * {
+        *   새로 들어온 문자 업데이트, check값 변경
+        * }
+        * Remove(문제 제거)
+        * {
+        *   제거된 문자 업데이트, check값 변경
+        * }
+        * */
+    }
+}
