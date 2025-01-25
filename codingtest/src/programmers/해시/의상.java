18f7e5dba6f04290ad71c9c758d55e56068974e1
minyongP <86906386+Minon98@users.noreply.github.com>
2025-01-25 23:24:31 +0900

    25/01/25-CT2

---
+++ codingtest/src/programmers/해시/의상.java
@@ -0,0 +1,22 @@
+package programmers.해시;
+
+public class 의상 {
+    public static void main(String[] args) {
+        의상 solution = new 의상();
+        System.out.println(solution(new String[][]{
+                {"yellow_hat", "headgear"},
+                {"blue_sunglasses", "eyewear"},
+                {"green_turban", "headgear"}
+        }));
+        System.out.println(solution(new String[][]{
+                {"crow_mask", "face"},
+                {"blue_sunglasses", "face"},
+                {"smoky_makeup", "face"}
+        }));
+    }
+
+    private static int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+    }
+}
--- codingtest/src/programmers/행렬의_곱셈.java
+++ codingtest/src/programmers/행렬의_곱셈.java
@@ -12,16 +12,42 @@ import java.util.*;
 public class 행렬의_곱셈 {
     public static void main(String[] args) {
         행렬의_곱셈 solution = new 행렬의_곱셈();
-        System.out.println(Arrays.toString(solution(new int[][]{{1,4}, {3,2}, {4,1}}, new int[][]{{3,3}, {3,3}})));
-        System.out.println(Arrays.toString(solution(new int[][]{{2,3,2}, {4,2,4}, {3,1,4}}, new int[][]{{5,4,3}, {2,4,1}, {3,1,1}})));
+        System.out.println(Arrays.deepToString(solution(new int[][]{
+                {1,4},
+                {3,2},
+                {4,1}
+        }, new int[][]{
+                {3,3},
+                {3,3}
+        })));
+        System.out.println(Arrays.deepToString(solution(new int[][]{
+                {2,3,2},
+                {4,2,4},
+                {3,1,4}
+        }, new int[][]{
+                {5,4,3},
+                {2,4,1},
+                {3,1,1}
+        })));
+        System.out.println(Arrays.deepToString(solution(new int[][]{
+                {1,2,3,4},
+                {1,2,3,4}
+        }, new int[][]{
+                {1,2},
+                {1,2},
+                {1,2},
+                {1,2}
+        })));
     }
 
     private static int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr1[0].length];
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
         for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
             }
         }
 
